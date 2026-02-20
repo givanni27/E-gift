9b1d20fbab3fecebda5ee90d630d6b85f86505eb
givanni27 <110678153+givanni27@users.noreply.github.com>
2026-02-20 10:05:28 +0800

    Update cek_login_users.php

--- Users/cek_login_users.php
+++ Users/cek_login_users.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 1); // Munculkan error jika ada
 
-// include database
 include '../Admin/database.php';
 
 $Password = $_POST['Password'];
@@ -10,18 +11,20 @@ $Email = $_POST['Email'];
 $query = "SELECT * FROM users WHERE email= '$Email' AND password = '$Password'";
 $sql = mysqli_query($koneksi, $query);
 
-// mengecek data
+if (!$sql) {
+    die("Query Error: " . mysqli_error($koneksi)); // Cek apakah tabel/kolom salah nama
+}
+
 $cek = mysqli_num_rows($sql);
 
 if ($cek > 0) {
     $row = mysqli_fetch_array($sql);
-    
-    // Simpan data ke session
     $_SESSION['id_users'] = $row['id_users'];
     $_SESSION['username_users'] = $row['username_users'];
     $_SESSION['email'] = $row['email'];
     $_SESSION['berhasil'] = true;
 
+    echo "Login Berhasil, mencoba pindah halaman..."; 
     header('location:index.php');
     exit();
 } else {
