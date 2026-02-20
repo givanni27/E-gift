37bb7c59b7c0f64bd75713de0b07608d12a88b4d
givanni27 <110678153+givanni27@users.noreply.github.com>
2026-02-20 10:28:27 +0800

    Update cek_login_users.php

--- Users/cek_login_users.php
+++ Users/cek_login_users.php
@@ -1,28 +1,31 @@
 <?php
 session_start();
-include ('../Admin/database.php');
+
+// include database
+include '../Admin/database.php';
 
 $Password = $_POST['Password'];
 $Email = $_POST['Email'];
 
 $query = "SELECT * FROM users WHERE email= '$Email' AND password = '$Password'";
 $sql = mysqli_query($koneksi, $query);
+
+// mengecek data
 $cek = mysqli_num_rows($sql);
 
 if ($cek > 0) {
     $row = mysqli_fetch_array($sql);
+    
+    // Simpan data ke session
     $_SESSION['id_users'] = $row['id_users'];
     $_SESSION['username_users'] = $row['username_users'];
     $_SESSION['email'] = $row['email'];
     $_SESSION['berhasil'] = true;
-    header('location:index.php'); // Pastikan tidak ada enter di atas baris ini
-    exit(); 
+
+    // Redirect ke index.php (tanpa folder Users karena file ini satu level)
+    header('Location: index.php');
+    exit();
 } else {
-    header('location:login.php?pesan=gagal');
+    header('Location: login.php?pesan=gagal');
     exit();
 }
-
-
-
-
-
