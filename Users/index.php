c51d6dcf384f8c0472ca95bc79ce5539d525d894
givanni27 <110678153+givanni27@users.noreply.github.com>
2026-02-20 09:20:48 +0800

    Update index.php

--- Users/index.php
+++ Users/index.php
@@ -5,7 +5,6 @@ if ($_SESSION['id_users'] != true) {
     header('location:login.php');
 }
 
-
 include('../Admin/database.php')
 
 <!DOCTYPE html>
@@ -666,4 +665,5 @@ if (isset($_GET['checkout']) && $_GET['checkout'] == 'success' && isset($_GET['i
 
 
 </html>
+
 
