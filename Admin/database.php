88d1934d10ef745634df26a0100daca11a33cdfb
givanni27 <110678153+givanni27@users.noreply.github.com>
2026-02-20 08:10:17 +0800

    Update database.php

--- Admin/database.php
+++ Admin/database.php
@@ -4,11 +4,11 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // Railway biasanya kasih variable ini otomatis kalau kita pake MySQL Railway
-$host = getenv('MYSQLHOST') ?: 'db'; 
-$user = getenv('MYSQLUSER') ?: 'db';
-$pass = getenv('MYSQLPASSWORD') ?: 'db';
-$db   = getenv('MYSQLDATABASE') ?: 'db';
-$port = getenv('MYSQLPORT') ?: '3306';
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD'); 
+$db   = getenv('MYSQLDATABASE'); 
+$port = getenv('MYSQLPORT');
 
 $conn = mysqli_connect($host, $user, $pass, $db, $port);
 
@@ -16,3 +16,4 @@ if (!$conn) {
     die("Aduh, koneksi database gagal: " . mysqli_connect_error());
 }
 ?>
+
