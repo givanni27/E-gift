fc107f0173e930f6559c9dc02319991685023806
givanni27 <110678153+givanni27@users.noreply.github.com>
2026-04-05 13:18:45 +0800

    Update index.php

--- Admin/index.php
+++ Admin/index.php
@@ -44,7 +44,7 @@ include('database.php'); // Path yang sudah diperbaiki
 
     <!-- Preloader -->
     <div class="preloader flex-column justify-content-center align-items-center">
-      <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+      <img class="animation__shake" src="./image/logo/liu.jpg" alt="AdminLogo" height="60" width="60">
     </div>
 
     <!-- Navbar -->
