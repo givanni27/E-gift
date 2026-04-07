86835c5897219dfaae3a0502557defb1ef11e139
givanni27 <110678153+givanni27@users.noreply.github.com>
2026-04-07 13:42:12 +0800

    Add files via upload

---
+++ Admin/proses/proses_tambah_akun.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+include('../database.php');
+
+if (!isset($_SESSION['id_admin']))
+    exit(header('Location: login.php'));
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['proses'])) {
+    extract($_POST);
+
+    $name = mysqli_real_escape_string($koneksi, $name);
+    $sql = ($proses === 'tambah')
+        ? "INSERT INTO users (username_users, password, email) VALUES ('$username_users', '$password', '$email')"
+        : "UPDATE users SET username_users='$username_users', password='$password', email='$email' WHERE id_users='$id_users'";
+
+    mysqli_query($koneksi, $sql);
+    header("location:../index.php?menu=daftar_akun");
+}
+
+// 4. Logika Delete
+if (isset($_GET['proses']) && $_GET['proses'] === 'delete') {
+    $id = $_GET['id_users'];
+    $data = mysqli_fetch_assoc(mysqli_query($koneksi, "SELECT * FROM users WHERE id_users = '$id'"));
+    mysqli_query($koneksi, "DELETE FROM users WHERE id_users = '$id'");
+    header("location:../index.php?menu=daftar_akun");
+}
