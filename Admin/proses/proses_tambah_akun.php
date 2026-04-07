d9acbea292a0f1a672ff4bc19bb36b5eedd959b9
givanni27 <110678153+givanni27@users.noreply.github.com>
2026-04-07 14:04:06 +0800

    Update proses_tambah_akun.php

--- Admin/proses/proses_tambah_akun.php
+++ Admin/proses/proses_tambah_akun.php
@@ -20,7 +20,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['proses'])) {
 // 4. Logika Delete
 if (isset($_GET['proses']) && $_GET['proses'] === 'delete') {
     $id_users = $_GET['id_users'];
-    $data = mysqli_fetch_assoc(mysqli_query($koneksi, "SELECT * FROM users WHERE id_users = '$id_users'"));
     mysqli_query($koneksi, "DELETE FROM users WHERE id_users = '$id_users'");
     header("location:../index.php?menu=daftar_akun");
 }
